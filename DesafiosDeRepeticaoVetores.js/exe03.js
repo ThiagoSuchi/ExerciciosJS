e2a9ee275a2ef8d88a1c86125c370eebc845b2e6
THIAGO <thiagohenssuchi@gmail.com>
2024-09-04 17:57:51 -0400

    xObjetos add

---
+++ DesafiosDeRepeticaoVetores.js/exe03.js
@@ -0,0 +1,17 @@
+/* 03 -     
+            IMC Classificação
+        menos que 16 Magreza grave
+        16 a 17 Magreza moderada
+        17 a 18,5 Magreza leve
+        18,5 a 25 Saudável
+        25 a 30 Sobrepeso
+        30 a 35 Obesidade GrauI
+        35 a 40 Obesidade GrauII(severa)
+        mais que 40 Obesidade GrauIII(mórbida) 
+
+    Faça um programa que receba a massa e a altura do usuário via argumentos da linha de
+comando e exiba a classificação do IMC dessa pessoa com base na tabela fornecida.
+Obs: Deve fazer utilizando uma matriz para guardar as informações da tabela acima, então vá
+passando pela lista com imc’s e caso encontre um valor que é maior que o imc calculado, exibe
+a última classificação. obs(IMC = Massa / (Altura * Altura)) */
+
